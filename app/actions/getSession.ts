47ed1e655f602604566c58cc0a65f0fb1fd497c2
DanielH987 <hootinid@go.byuh.edu>
2024-03-20 14:58:00 -1000

    update

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
 export default async function getSession() {
     return await getServerSession(authOptions);
