d791544e2f89bcd567fb60890644e6bbfaff2e57
DanielH987 <hootinid@go.byuh.edu>
2024-04-25 13:42:30 -1000

    Update

--- app/components/sidebar/MenuButton.tsx
+++ app/components/sidebar/MenuButton.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const MenuButton = ({ toggleDrawer }) => {
+interface MenuButtonProps {
+    toggleDrawer?: () => void;
+}
+
+const MenuButton: React.FC<MenuButtonProps> = ({ toggleDrawer }) => {
     return (
         <button
             className="fixed bottom-0 left-0 m-4 p-2 z-50 bg-[#FFF1EC] border-[1px] rounded lg:hidden shadow hover:bg-[#EDE0DA]"
